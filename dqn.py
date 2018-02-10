2902b229723002204e2775fb191e1b7e67e4ff3d
tim54100 <theworld.sthelastestotaku@gmail.com>
2018-02-10 23:59:51 +0800

    Update dqn.py

--- dqn.py
+++ dqn.py
@@ -4,12 +4,13 @@
 import numpy as np
 from DQN_N import DeepQNetwork
 import sys
-#sys.path.append("/home/eastgeno/workspace/gym")
-#sys.path.append("/home/eastgeno/anaconda2/lib/python2.7/site-packages")
+# sys.path.append("/home/eastgeno/workspace/gym")
+# sys.path.append("/home/eastgeno/anaconda2/lib/python2.7/site-packages")
 import cv2
 import pyglet
 import tensorflow as tf
 import gym
+import matplotlib.pyplot as plt
 from gym.wrappers import SkipWrapper
 from scipy.misc import imresize
 from gym.core import ObservationWrapper
@@ -56,24 +57,28 @@ if __name__ == "__main__":
             str(env.observation_space)[4:-1].split(','),
             learning_rate= 0.00025,
             reward_decay =0.99,
-            epsilon_start= 0.1,
+            epsilon_start= 1,
             epsilon_end = 0.1,
             explore = 1000000,
             replace_target_iter=10000,
             memory_size= 1000000,
             batch_size = 32,
             training = True,
+            output_graph = True,
             )
     #env = wrappers.Monitor(env, './experiment', force=True)
     step = 0
     loss_step =0
     RL.epsilon_decrease=False
     num_game = 0
+    e_step = []
+    e_score = []
+    epsilon = []
     while(True):
         loss = 0
         loss_m =0
         loss_step /=100
-        for episode in range(151):
+        for episode in range(1000):
             state = env.reset()
             
         #state = cv2.cvtColor(cv2.resize(state, (80, 80)), cv2.COLOR_BGR2GRAY)
@@ -92,8 +97,8 @@ if __name__ == "__main__":
 
             while not done:
                 #env.render()
-                action_probs = RL.make_policy(state)
-                action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+                action = RL.make_policy(state)
+                #action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
                 #action = RL.make_policy(state)
                 #print(action_probs)
                 #s =False
@@ -141,7 +146,7 @@ if __name__ == "__main__":
                     if step %100 == 0:
                         print('total_step: %d, step: %d' % (step, i*4))'''
                 n_state = np.append(state[:, :, 1:], np.expand_dims(n_state, 2), axis=2)
-                    #n_done = 1 if done else 0
+                   # n_done = 1 if done else 0
                 if info['ale.lives'] != lives:
                     RL.store_transition(state, action, -1, n_state, True)
                     #lives = info['ale.lives']
@@ -153,12 +158,13 @@ if __name__ == "__main__":
                 if (step > RL.memory_size and step%4 == 0):
                     loss += RL.learn()
                     i+=1
-                if step > 50000:
+                if step > RL.memory_size/10:
                     RL.epsilon_decrease=True
                 if step %100 == 0:
                      print('total_step: %d, step: %d' % (step, i*4))
                 
             loss = loss/i if (i != 0 ) else 0
+
             '''if episode % 10 == 0:
                 loss_m =loss
             if loss_m != 0 and loss != 0 and RL.lr > 1e-06 and loss < 100 and loss >= loss_m and RL.lr != 1e-08:
@@ -177,9 +183,20 @@ if __name__ == "__main__":
             print('episode: %d, epsilon: %f, total_reward: %d, loss: %f'%(num_game+episode+1, RL.epsilon, total_reward, loss))
             #print('episode: %d, epsilon: %f, total_reward: %d, lr: %s'%(num_game+episode+1, RL.epsilon, total_reward, str(RL.lr)))
             print('action'+'action'.join(str(i)+': '+str(action_tracker[i])[:-2]+'  ' for i in range(len(action_tracker))))
-        '''if RL.epsilon == RL.epsilon_end and RL.epsilon_end == 0.2:
-            RLepsilon_start= RL.epsilon_end
-            RL.explore = 10000000
-            RL.epsilon_end = 0.001'''
-        num_game+=151
+            if loss != 0:
+                e_step.append(RL.learn_step_counter)
+                e_score.append(total_reward)
+                epsilon.append(RL.epsilon)
+                
+        if RL.epsilon == RL.epsilon_end and RL.epsilon_end == 0.1:
+            RL.explore = 5000000
+            RL.epsilon_end = 0.01
+        
+        num_game+=1000
+        if RL.learn_step_counter != 0:
+
+            plt.plot(e_step, e_score)
+            plt.ion()#本次运行请注释，全局运行不要注释
+            plt.savefig('./picture/'+str(RL.learn_step_counter)+'.png')
+            #plt.show()
 # RL.plot_cost()
