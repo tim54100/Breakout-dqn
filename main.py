51d3e79eb001ba6a3cc661afb01ba7ade2251c0d
tim54100 <theworld.sthelastestotaku@gmail.com>
2019-09-12 13:49:14 +0800

    add show.ipynb to show history of training and let agent play game

--- main.py
+++ main.py
@@ -1,224 +1,190 @@
+
 # coding: utf-8
+
+# In[1]:
 
 
 import numpy as np
-from DQN_N import DeepQNetwork
-import sys
-# sys.path.append("/home/eastgeno/workspace/gym")
-# sys.path.append("/home/eastgeno/anaconda2/lib/python2.7/site-packages")
-import cv2
 import os
-import pyglet
-import tensorflow as tf
-import gym
+import random
+import sys
+import cv2
+import time
+
+import pickle
 import matplotlib.pyplot as plt
-from gym.wrappers import SkipWrapper
-from scipy.misc import imresize
-from gym.core import ObservationWrapper
-from gym.spaces.box import Box
-from gym import wrappers
-from PIL import Image
+import gym
+from tqdm import tqdm
+from DQN_N import DeepQNetwork
 
-class PreprocessImage(ObservationWrapper):
-    def __init__(self, env, height=64, width=64, grayscale=True,
-                 crop=lambda img: img):
-        """A gym wrapper that crops, scales image into the desired shapes and optionally grayscales it."""
-        super(PreprocessImage, self).__init__(env)
-        self.img_size = (height, width)
-        self.grayscale = grayscale
-        self.crop = crop
 
-        n_colors = 1 if self.grayscale else 3
-        self.observation_space = Box(0.0, 1.0, [n_colors, height, width])
+# In[2]:
 
-    def _observation(self, img):
-        """what happens to the observation"""
-        img = self.crop(img)
-        img = imresize(img, self.img_size)
-        if self.grayscale:
-            img = img.mean(-1, keepdims=True)
-        img = np.transpose(img, (2, 0, 1))  # reshape from (h,w,colors) to (colors,h,w)
-        img = img.astype('float32') / 255.
-        img = np.squeeze(img)
-        return img
 
-def make_env():
-    env_spec = gym.spec('Breakout-v0')
-    env_spec.id = 'Breakout-v0'
-    env = env_spec.make()
-    e = PreprocessImage(SkipWrapper(4)(env),
-                                 width=84, height=84, grayscale=True)
-    return e
+def pltin(x, y, x_name, y_name, file_path):
+    plt.figure()
+    plt.plot(x, y,'--*b')
+    plt.xlabel(x_name)
+    plt.ylabel(y_name)
+    plt.savefig(file_path)
 
-if __name__ == "__main__":
-    #creat a env,the example is Breakout-v0
 
-    env = make_env()
-    env = wrappers.Monitor(env, './experiment', force=True)
-    RL = DeepQNetwork(env.action_space.n,
-            str(env.observation_space)[4:-1].split(','),
-            learning_rate= 0.00025,
-            reward_decay =0.99,
-            epsilon_start= 1,
-            epsilon_end = 0.1,
-            explore = 10000000,
-            replace_target_iter=10000,
-            memory_size= 1000000,
-            batch_size = 32,
-            training = True,
-            output_graph = True,
-            )
-    #env = wrappers.Monitor(env, './experiment', force=True)
-    step = 0
-    loss_step =0
-    RL.epsilon_decrease=False
-    num_game = 0
-    e_step = []
-    e_score = []
-    epsilon = []
-    e_loss = []
-    push = False
-    while(True):
-        loss = 0
-        loss_m =0
-        loss_step /=100
-        total_score = 0
-        total_loss = 0
-        for episode in range(100):
-            state = env.reset()
-            
-            img = Image.fromarray(np.array(state), 'L')
-            img.show()	
-        #state = cv2.cvtColor(cv2.resize(state, (80, 80)), cv2.COLOR_BGR2GRAY)
-        #ret, state = cv2.threshold(state, 1, 255, cv2.THRESH_BINARY)
-        #state = np.reshape(state, (80, 80))
-            state = np.stack((state, state, state, state), axis=2)
-        #print(state.shape)
+# In[3]:
+
+
+def skip_step(frames_to_skip, env, step, action):
+    reward = 0
+    for i in range(frames_to_skip):
+        state, r, done, info = step(action)
+        reward += r 
+        if done:
+            break
+    return state, reward, done, info
+
+
+# In[4]:
+
+
+def preprocess(img, width, height):
+    #img = cv2.cvtColor(cv2.resize(img, (width, length)), cv2.COLOR_RGB2GRAY)
+    img = np.array(img, dtype = np.uint8)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (width, height))
+#     img = np.vstack(img)
+    img = img.astype(np.float)
+    img /= 255
+    
+    return img
+
+
+# In[5]:
+
+
+env = gym.make('MsPacman-v0')# Breakout-ram-v0
+
+width = 84
+height = 84
+batch_size = 32
+memory_size = 80000
+replace_iter = 4000
+frame_repeat = 4
+n_a = env.action_space.n
+push = False
         
-            action_tracker=np.zeros(env.action_space.n)
-            score=0
+step = 0    
+e_step = []
+e_score = []
+e_loss = []
+epsilon = []
+episodes = 100
+num_game = 0
+
+RL = DeepQNetwork(n_a,
+                  st_shape = [width, height],
+                  learning_rate= 0.0001,
+                  reward_decay =0.99,
+                  epsilon_start= 1,
+                  epsilon_end = 0.1,
+                  explore = 500000,
+                  replace_target_iter= replace_iter,
+                  memory_size= memory_size,
+                  batch_size = batch_size,
+                  training = True,
+                  output_graph = False,)
+
+
+# In[6]:
+
+
+while(True):
+    total_loss = 0
+    total_score = 0
+    with tqdm(total=episodes) as t:
+        for episode in range(episodes):
+
+
+            action_tracker = np.zeros(n_a)
+            loss = 0
+            i=0
+            score = 0
             done = False
             lives = -1
-            i = 0
             info = {}
-            info['ale.lives'] = 5
+#             info['ale.lives'] = 5
+            info['ale.lives'] = 3
+            
+            s = env.reset()
+#             s = np.array(s,dtype='float')
+            s = preprocess(s, width, height)
+            s = np.stack((s, s, s, s), axis=2)
+#                 print(s.shape)
 
             while not done:
-                #env.render()
-                action = RL.make_policy(state)
-                #action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
-                #action = RL.make_policy(state)
-                #print(action_probs)
-                #s =False
+                action = RL.make_policy(s)
+                
                 if info['ale.lives'] != lives:
                     lives = info['ale.lives']
-                    action=1
+#                     action=1
                     #s = True
-                action_tracker[action]+=1
-                
-                n_state, reward, done, info = env.step(action)
-            #n_state = cv2.cvtColor(cv2.resize(n_state, (80, 80)), cv2.COLOR_BGR2GRAY)
-            #ret, n_state = cv2.threshold(n_state, 1, 255, cv2.THRESH_BINARY)
-            #n_state = np.reshape(n_state, (80, 80, 1))
-            #print(info['ale.lives'])
-                score+=reward
-                #if reward == 0:
-                #    reward=-0.01
-                #else:
-                #    reward/=1000
-                #if info['ale.lives'] != lives or done:
-                #    lives = info['ale.lives']
-                #    reward-=9
-                #    action_tracker[action]-=1
-                #    action = 1
-                #reward/=4
-                #if lives == -1:
-                #    lives = info['ale.lives']
-                '''if s:
-                    state = n_state
-                    state = np.stack((state, state, state, state), axis=2)
-                else:
-                    n_state = np.append(state[:, :, 1:], np.expand_dims(n_state, 2), axis=2)
-                   # n_done = 1 if done else 0
-                    if info['ale.lives'] != lives:
-                        RL.store_transition(state, action, -1, n_state, True)
-                    else:
-                        RL.store_transition(state, action, reward, n_state, done)
-                    step+=1
-                   
-                    state=n_state
-                    if (step > 50000 and step%4 == 0):
-                        loss += RL.learn()
-                        RL.epsilon_decrease=True
-                        i+=1
-                    if step %100 == 0:
-                        print('total_step: %d, step: %d' % (step, i*4))'''
-                n_state = np.append(state[:, :, 1:], np.expand_dims(n_state, 2), axis=2)
-                   # n_done = 1 if done else 0
+                    
+                action_tracker[action] += 1
+                n_s, reward, done, info = skip_step(frame_repeat, env, env.step, action)
+#                 n_s = np.array(n_s,dtype='float')
+                n_s = preprocess(n_s, width, height)
+                n_s = np.append(s[:, :, 1:], np.expand_dims(n_s, 2), axis=2)
+                score += reward
+                    
                 if info['ale.lives'] != lives:
-                    RL.store_transition(state, action, -1, n_state, True)
-                    #lives = info['ale.lives']
-                else:
-                    RL.store_transition(state, action, reward, n_state, done)
-                step+=1
-               
-                state=n_state
-                if (step > RL.memory_size and step%4 == 0):
-                    loss += RL.learn()
-                    i+=1
-                if step > RL.memory_size*0.8:
-                    RL.epsilon_decrease=True
-                if step %100 == 0:
-                     print('total_step: %d, step: %d' % (step, i*4))
-                
-            loss = loss/i if (i != 0 ) else 0
+                    reward = -10
+                    #s = True
+                    
+                reward /= 10
+                reward = np.max([-1 , np.min([1, reward])])
+                RL.store(s, action, reward, n_s, not done)
 
+                s = n_s
+                if (len(RL.replay_memory) > RL.memory_size/4 and step%4 == 0):
+                    los = RL.learn()
+#                     print(los)
+#                     los,TD,p_loss = RL.learn()
+#                     print("TD: %f, p_loss: %f"%(np.reducemean(TD), p_loss))
+                    loss += los
+                    i+=1
+                step += 1
+#                     if step %1000 == 0:
+#                          print('total_step: %d, step: %d, current_score: %f' % (step, i*4, score))
+
+            loss = loss/i if (i != 0 ) else 0
             total_score += score
             total_loss += loss
-            '''if episode % 10 == 0:
-                loss_m =loss
-            if loss_m != 0 and loss != 0 and RL.lr > 1e-06 and loss < 100 and loss >= loss_m and RL.lr != 1e-08:
-                loss_step += 1
-                if loss_step >=10:
-                    RL.lr /= 10
-                    loss_step = -10
-            elif loss*1.1 < loss_m and RL.lr != 1e-08:
-                loss_step -= 1
-                if loss_step <=-10 and RL.lr <0.00001:
-                    #RL.lr *= 10
-                    loss_step = 0'''
+#                 print('episode: %d, epsilon: %f, score: %f, loss: %f'%(num_game+episode+1, RL.epsilon, score, loss))
+#                 print('action'+'action'.join(str(i)+': '+str(action_tracker[i])[:-2]+'  ' for i in range(len(action_tracker))))
 
-            #print('episode: %d, epsilon: %f, score: %d, loss: %f, lr: %s'%(num_game+episode+1, RL.epsilon, \
-            #      score, loss, str(RL.lr)))
-            print('episode: %d, epsilon: %f, score: %d, loss: %f'%(num_game+episode+1, RL.epsilon, score, loss))
-            #print('episode: %d, epsilon: %f, score: %d, lr: %s'%(num_game+episode+1, RL.epsilon, score, str(RL.lr)))
-            print('action'+'action'.join(str(i)+': '+str(action_tracker[i])[:-2]+'  ' for i in range(len(action_tracker))))
-            
-                
-        if RL.epsilon == RL.epsilon_end and RL.epsilon_end == 0.1:
-            RL.epsilon_start = 0.1
-            RL.explore = 10000000
-            RL.epsilon_end = 0.01
-        
-        num_game+=1000
-        if push:
-            if not os.path.exists('picture'):
-                os.makedirs('picture')
-            e_step.append(RL.learn_step_counter/10000)
-            e_score.append(total_score/100)
-            epsilon.append(RL.epsilon)
-            e_loss.append(total_loss/100)
-            plt.figure()
-            plt.plot(e_step, e_score,'--*b')
-            plt.xlabel('step')
-            plt.ylabel('score')
-            plt.savefig('./picture/'+str(RL.learn_step_counter)+'_score.png')
-            plt.figure()
-            plt.plot(e_step, e_loss,'--*b')
-            plt.xlabel('step')
-            plt.ylabel('loss')
-            plt.savefig('./picture/'+str(RL.learn_step_counter)+'_loss.png')
-            plt.close('all')
-        if total_loss!=0:
-            push = True
-# RL.plot_cost()
+
+            t.set_postfix(score=score, loss = loss, step=step)
+            t.update()
+#         if RL.epsilon == RL.epsilon_end and RL.epsilon_end != 0.001:
+#             RL.learning_rate= 0.0001
+#             RL.epsilon_start /= 10
+#             RL.explore = 2000000
+#             RL.epsilon_end /= 10
+
+        num_game += episodes
+
+    if push:
+        e_step.append(RL.learn_step_counter/10000)
+        e_score.append(total_score/episodes)
+        epsilon.append(RL.epsilon)
+        e_loss.append(total_loss/episodes)
+        path = './picture/'+str(RL.learn_step_counter)
+        file = open('./saved_networks/train_history.pickle', 'wb')
+        pickle.dump([e_step, e_score, epsilon, e_loss], file)
+        file.close()
+#         pltin(e_step, e_score, 'step(10000)' , 'score', path + '_score.png')
+#         pltin(e_step, e_loss, 'step(10000)' , 'loss', path + '_loss.png')
+#         plt.close('all')
+    if total_loss != 0:
+        push = True
+#         if not os.path.exists('./picture'):
+#             os.makedirs('picture')
+
